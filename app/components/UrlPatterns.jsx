9480c8fd72563109cf85475191b72cef0c0446c2
Sam <msalman233@gmail.com>
2024-05-25 16:51:24 +0500

    Update domain

--- app/components/UrlPatterns.jsx
+++ app/components/UrlPatterns.jsx
@@ -1,3 +1,5 @@
-const baseUrl = "http://localhost:8000/meetApi/"
+// const baseUrl = "http://localhost:8000/meetApi/"
+const baseUrl = "https://meetaicoders.com/meetApi/"
+
 
 export default baseUrl;
