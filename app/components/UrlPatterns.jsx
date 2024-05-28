723e4e01adadcec939f0d7e6906b2fa79807315b
Sam <msalman233@gmail.com>
2024-05-28 14:37:37 +0500

    update url patterns

--- app/components/UrlPatterns.jsx
+++ app/components/UrlPatterns.jsx
@@ -1,5 +1,5 @@
-const baseUrl = "http://localhost:8000/meetApi/"
-// const baseUrl = "https://meetaicoders.com/meetApi/"
+// const baseUrl = "http://localhost:8000/meetApi/"
+const baseUrl = "https://meetaicoders.com/meetApi/"
 
 
 export default baseUrl;
